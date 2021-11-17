8ffb3391315652bf1420100d6bde4861d08771e6
Bee <reinsaway@gmail.com>
2021-11-16 20:08:37 -0800

    fix SideMenu, add buttons Log Out/Add Class

---
+++ src/components/SideMenu/SideMenu.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Image, Menu, Button } from 'semantic-ui-react';
+import './Menu.css'
+
+
+export default function SideMenu({ handleLogout }) {
+
+    return (
+        <Menu inverted pointing vertical  style={{height: '100vh', width: '50vh', border: 'solid', color:'white'}} className='SideMenu'>
+            <Menu.Header>
+                <Link to="/"><Image src="../Grade.Studio1(2).gif" size='huge' /></Link>
+            </Menu.Header>
+            <Menu.Item style={{ border: 'solid'}}>
+                <Button inverted color='white' onClick={handleLogout} size='large' style={{width: '20vh'}}> Log Out </Button>
+                <Button inverted color='white' size='large' style={{width: '20vh'}} floated='right'>Add a Class</Button>
+            </Menu.Item>
+        </Menu>
+    )
+}
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,13 +1,11 @@
 import { Grid, Menu } from "semantic-ui-react";
 import { Outlet } from "react-router-dom";
-import SideMenu from "../../components/Menu/Menu";
+import SideMenu from "../../components/SideMenu/SideMenu";
 
 export default function Layout({ user, handleLogout }) {
     return (
-        <Grid>
-            <Grid.Row>
+        <>
                 <SideMenu handleLogout={handleLogout} />
-            </Grid.Row>
-        </Grid>
+        </>
     );
 }
