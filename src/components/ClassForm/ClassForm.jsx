82933c25538954fdb2913ea3d3a3d991501261c8
Bee <reinsaway@gmail.com>
2021-11-18 10:39:15 -0800

    Add ClassForm functionality

--- src/components/ClassForm/ClassForm.jsx
+++ src/components/ClassForm/ClassForm.jsx
@@ -11,26 +11,72 @@ import {
     Segment,
 } from "semantic-ui-react";
 
+
 export default function ClassForm() {
+
+    const [state, setState] = useState({
+        name: "",
+        students: [""]
+    })
+
+    function submitClass(){
+        console.log(JSON.stringify(state));
+    }
+
+    function formHandler(eventObject) {
+        const { name, value } = eventObject.target
+        if (name === "name") {
+            setState({
+                ...state, name: value
+            })
+        } else {
+            const [, indexString] = name.split(" ")
+            const index = Number(indexString)
+            const newStudentsArray = [...state.students]
+            newStudentsArray[index] = value
+            setState({ ...state, students: newStudentsArray })
+        }
+    }
+
+    function addStudentHandler() {
+        setState({...state, students: [...state.students,""]})
+    }
+
+    function deleteStudentHandler(index) {
+        const newStudentsArray = [...state.students]
+        newStudentsArray.splice(index,1)
+        setState({ ...state, students: newStudentsArray })
+    }
+
+    const disableSubmit = (state.students[state.students.length - 1].trim() === "") || (state.name.trim() === "")
+
     return (
-    <div>
-    <Form>
-        <Form.Input
-        name="name"
-        placeholder="Class Name"
-        value={state.name}
-        />
-    </Form>
-    <Form>
-        <h3>Add your Students!</h3>
-        <Form.Input
-        name="name"
-        placeholder="Student Name"
-        value={state.name}
-        />
-    </Form>    
-        <Button type ="submit" className="btn">
-        Submit New Class
-        </Button>
-    </div>    
-)}
+        <div>
+            <Form>
+                <Form.Input
+                    name="name"
+                    placeholder="Class Name"
+                    value={state.name}
+                    onChange={formHandler}
+                />
+                {state.students.map((name, index) => {
+                    
+                    return (<><div style={{ display: 'flex', flexDirection: 'row-reverse' }}><Form.Input
+                        name={`students ${index}`}
+                        placeholder="Student Name"
+                        value={name}
+                        onChange={formHandler}
+                    />
+                    <div style={{ position: 'absolute'  }}><Button  floated='right' onClick={() => deleteStudentHandler(index)}>X</Button></div>
+                    </div>{(index === state.students.length - 1 && name.trim() !== "") &&
+                     <Button onClick={addStudentHandler}>Add Another Student</Button>}
+                     
+                     </>)
+                })}
+            </Form>
+            <Button onClick={submitClass} disabled={disableSubmit} className="btn">
+                Submit New Class
+            </Button>
+        </div>
+    )
+}
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -2,11 +2,13 @@ import { Grid, Menu, Container } from "semantic-ui-react";
 import { Outlet } from "react-router-dom";
 import SideMenu from "../../components/SideMenu/SideMenu";
 import WebsiteDescription from "../../components/WebsiteDescription/WebsiteDescription";
+import ClassForm from "../../components/ClassForm/ClassForm";
 
 export default function Layout({ user, handleLogout }) {
     return (
         <div style={{ display: 'flex' }}>
             <SideMenu handleLogout={handleLogout} />
+            <ClassForm />
             <Outlet />
         </div>
     )
