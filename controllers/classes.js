cc50ced09fb6086dcfefbf97f431d09c8f6d3a93
Bee <reinsaway@gmail.com>
2021-11-18 13:41:28 -0800

    Add newStudents to classes controller

--- controllers/classes.js
+++ controllers/classes.js
@@ -1,4 +1,5 @@
-const Class = require('../models/class')
+const Class = require('../models/class');
+const Student = require('../models/student')
 
 
 module.exports = { create };
@@ -8,11 +9,16 @@ async function create(req, res) {
 
   try {
     console.log(req.body, req.user);
+    const { name, students } = req.body
+    const newStudents = await students.map(student => Student.create({
+      name: student
+    }))
     const newClass = await Class.create({
-      name: req.body.name,
-      teacher: req.user
+      name,
+      teacher: req.user,
+      students: newStudents
     });
-    res.json({status:200})
+    res.json({ status: 200 })
   } catch (err) {
     console.log(err);
     res.status(400).json({ err })
