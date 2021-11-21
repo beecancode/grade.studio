4f0ac08cad59755bbcfc8dfd9f3ac7651ed4d8ba
Bee <reinsaway@gmail.com>
2021-11-21 01:51:15 -0800

    Add steps for success

--- src/components/ClassForm/ClassForm.jsx
+++ src/components/ClassForm/ClassForm.jsx
@@ -3,17 +3,20 @@ import { addClass } from '../../utils/classService'
 import {
     Button,
     Form,
+    Header,
+    Step,
+    Icon,
 } from "semantic-ui-react";
 
 
-export default function ClassForm({getClasses}) {
+export default function ClassForm({ getClasses }) {
 
     const [state, setState] = useState({
         name: "",
         students: [""]
     })
 
-    async function submitClass(){
+    async function submitClass() {
         const response = await addClass(state)
         console.log(response);
         await getClasses(state)
@@ -36,48 +39,51 @@ export default function ClassForm({getClasses}) {
     }
 
     function addStudentHandler() {
-        setState({...state, students: [...state.students,""]})
+        setState({ ...state, students: [...state.students, ""] })
     }
 
     function deleteStudentHandler(index) {
         const newStudentsArray = [...state.students]
-        newStudentsArray.splice(index,1)
+        newStudentsArray.splice(index, 1)
         setState({ ...state, students: newStudentsArray })
     }
 
     const disableSubmit = (state.students[state.students.length - 1].trim() === "") || (state.name.trim() === "")
 
     return (
-        <div style={{ color:'white', border:'solid', textAlign:'center', padding: '10px', minWidth:'60vw'}}>
+        <div style={{ color: 'white', border: 'solid', textAlign: 'center', padding: '10px', minWidth: '60vw' }}>
+            <Header as='h1' style={{ color: 'white', border: 'solid', fontSize: '40px', marginTop: '15px', padding: '6px', fontFamily: 'Pangolin' }}>Add a New Class</Header>
             <Form >
-                <label style={{ color:'white', fontSize:'20px', marginTop:'15px' }}>Name of Class</label>
+                <label style={{ color: 'white', fontSize: '20px', marginTop: '15px' }}>Name of Class</label>
                 <Form.Input
                     name="name"
                     placeholder="Class Name"
                     value={state.name}
                     onChange={formHandler}
                 />
-                <label style={{ color:'white', fontSize:'20px', marginTop:'15px' }}>Student Name</label>
+                <label style={{ color: 'white', fontSize: '20px', marginTop: '15px' }}>Student Name</label>
                 {state.students.map((name, index) => {
-                    
-                    return (<><div style={{ display: 'flex', flexDirection: 'row-reverse', minWidth:'60vw' }}><Form.Input
+
+                    return (<><div style={{ display: 'flex', flexDirection: 'row-reverse', minWidth: '60vw' }}><Form.Input
                         name={`students ${index}`}
                         placeholder="Student Name"
                         value={name}
                         onChange={formHandler}
-                        style={{ minWidth:'60vw' }}
+                        style={{ minWidth: '60vw' }}
                     />
-                    <div style={{ position: 'absolute'  }}><Button  floated='right' onClick={() => deleteStudentHandler(index)}>X</Button></div>
+                        <div style={{ position: 'absolute' }}><Button floated='right' onClick={() => deleteStudentHandler(index)}>X</Button></div>
                     </div>
-                     
-                     
-                     </>)
+
+
+                    </>)
                 })}
             </Form>
             <Button inverted color="white" onClick={addStudentHandler}>Add Another Student</Button>
             <Button inverted color="white" onClick={submitClass} disabled={disableSubmit} className="btn">
                 Submit New Class
             </Button>
+            
+            
         </div>
     )
 }
--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Image, Menu, Button, Grid } from 'semantic-ui-react';
+import { Image, Menu, Button, Grid, Step, Header, Icon } from 'semantic-ui-react';
 import './Menu.css'
 import ClassList from '../ClassList/ClassList'
 
@@ -20,8 +20,8 @@ export default function SideMenu({ handleLogout, classes, showClassForm, setButt
             <Menu.Item style={{ border: 'solid' }}>
                 <Grid>
                     <Grid.Row style={{ justifyContent: 'space-evenly' }}>
-                        <Button inverted color='white' onClick={handleLogout} size='large' style={{ minWidth: '175px', height: 'auto' }}> Log Out </Button>
-                        <Button inverted color='white' onClick={handleButtonClick} size='large' style={{ minWidth: '175px', height: 'auto' }}>Add a Class</Button>
+                        <Button inverted color='facebook' onClick={handleLogout} size='large' style={{ minWidth: '175px', height: 'auto' }}> Log Out </Button>
+                        {/* <Button inverted color='white' onClick={handleButtonClick} size='large' style={{ minWidth: '175px', height: 'auto' }}>Add a Class</Button> */}
                     </Grid.Row>
                 </Grid>
             </Menu.Item>
@@ -35,6 +35,48 @@ export default function SideMenu({ handleLogout, classes, showClassForm, setButt
             }}>My Classes   
             </Menu.Header>
             <ClassList classes={classes ? classes : []} />
+            <Step.Group stackable vertical inverted style={{ maxWidth: '75vw' }}>
+                <Header as='h2' style={{ color: 'white', marginTop: '15px', marginBottom: 'auto', padding: '5px', fontFamily: 'Pangolin' }}>Grade.Studio Steps for Success</Header>
+                <Step active>
+                    <Icon name='pencil' />
+                    <Step.Content>
+                        <Step.Title>Add a Class</Step.Title>
+                        <Step.Content>
+                            <ol>
+                                <li>Add your class by name</li>
+                                <li>Add the names of all of your students.</li>
+                            </ol>
+                        </Step.Content>
+                    </Step.Content>
+                </Step>
+
+                <Step>
+                    <Icon name='paperclip' />
+                    <Step.Content>
+                        <Step.Title>Add assignments</Step.Title>
+                        <Step.Content>
+                            <ol>
+                                <li>Click on the desired class</li>
+                                <li>Add an assignment to be graded</li>
+                                The students are automatically populated!
+                            </ol>
+                        </Step.Content>
+                    </Step.Content>
+                </Step>
+
+                <Step >
+                    <Icon name='percent' />
+                    <Step.Content>
+                        <Step.Title>Grade an Assignment</Step.Title>
+                        <ol>
+                        <li>Choose an assignment to be graded</li>
+                        <li>Input the number of correct answers for each Student</li>
+                        <li>See your students' percentage grades automatically calculated</li>
+                        <li>Save them for safe keeping!</li>
+                        </ol>
+                    </Step.Content>
+                </Step>
+            </Step.Group>
 
         </Menu>
     )
