7a836cb0e63dd92d522febd4eac090226367639b
Bee <reinsaway@gmail.com>
2021-11-22 10:46:51 -0800

    Edit WebsiteDescription and first Screenshot

--- src/components/WebsiteDescription/WebsiteDescription.jsx
+++ src/components/WebsiteDescription/WebsiteDescription.jsx
@@ -9,7 +9,7 @@ export default function WebsiteDescription() {
             <p>Grade.Studio is an easy grading tool to make those long nights grading assignments a little easier!</p>
             <p>Once you have created an account, you can add to your list of classes, and add and edit assignments for each class.</p>
             <p>After clicking on a classes assignment, you can add in how many answers each student made correctly, and automatically calculate each students percentage grade all at once!</p>
-            <p>Best of all, once you have generated all the grades for an assignment, you have the option to automatically generate a .txt file with all your student’s grades!</p>
+            <p>Best of all, once you have generated all the grades for an assignment, you can save them for safe keeping (with the ability to edit of course)!</p>
             <Header as='h3' size='large' style={{ fontFamily: 'Pangolin', color: 'white' }}>Happy Grading!</Header>
         </Container>
     )
